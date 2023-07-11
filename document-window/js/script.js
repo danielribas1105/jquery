e34e4c55d649978cce32df76e33302c2f7847d11
Daniel <danielribas1105@gmail.com>
2023-07-10 21:54:05 -0300

    update project

--- document-window/js/script.js
+++ document-window/js/script.js
@@ -1,7 +1,19 @@
 $(document).ready(() => {
-    alert("DOM OK");
+    
 });
 
 $(window).on('resize', () => {
-    $('.width').text(window.innerWidth);
+    let width = window.innerWidth;
+    $('.width').text(width);
+    if(width < 1200){
+        let col = 2;
+        let margin = 20;
+        console.log(col + " colunas e margem de "+margin);
+        $('body').css('background-color', 'red');
+    }else {
+        let col = 3;
+        let margin = 40;
+        console.log(col + " colunas e margem de "+margin);
+        $('body').css('background-color', 'blue');
+    }
 })
